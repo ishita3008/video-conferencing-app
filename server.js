d9d5e84c68c88716696f3764ffcee2fd44f53830
ishita3008 <ishitalather_2k19it063@dtu.ac.in>
2021-06-22 21:45:58 +0530

    first_commit

---
+++ server.js
@@ -0,0 +1,9 @@
+//setting up the server
+const express = require('express')// create an express server
+const app = express() //runs the express function
+
+const server = require('http').Server(app) //setting up express server to be used with further libraries
+app.get('/',(req,res)=>{
+    res.status(200).send('HELLO');
+})
+server.listen(3000) //server will be a local host andd the port will be 3000 

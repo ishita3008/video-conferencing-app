df2c4929292bd988dda4a1a23819ee6d39863511
ishita3008 <ishitalather_2k19it063@dtu.ac.in>
2021-06-23 23:53:59 +0530

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 //setting up the server
-//..
-const express = require('express')// create an express server
-const app = express() //runs the express function
 
-const server = require('http').Server(app) //setting up express server to be used with further libraries
-app.get('/',(req,res)=>{
-    res.status(200).send('HELLO');
+const express = require('express');// create an express server
+const app = express(); //runs the express function
+const{v4: uuidV4}=require('uuid');//importing v4 version of uuid
+ //uuid would generate random ids for the rooms
+
+const server = require('http').Server(app); //setting up express server to be used with further libraries
+
+app.set('view engine', 'ejs');//our view engine is going to be ejs
+
+app.get('/', (req, res)=>{
+    res.redirect(`/${uuidV4()}`) //redirect to a page with a new gennerated id
 })
-server.listen(3000) //server will be a local host andd the port will be 3000 
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room }) //passed id to the front end
+  })
+
+server.listen(3010); //server will be a local host andd the port will be 3000 
+

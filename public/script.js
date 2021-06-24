88351341c0adc08bec43d4ad8b7ad875a8eadbee
ishita3008 <ishitalather_2k19it063@dtu.ac.in>
2021-06-24 18:38:13 +0530

    added_both_videos

--- public/script.js
+++ public/script.js
@@ -1,22 +1,64 @@
+
 // all javascript for the front end 
+const socket = io('/')//importing socket.io
 const theVideoGrid = document.getElementById('the_video_grid')//we will put the videos in this variable
 console.log(theVideoGrid);
 const ownVideo = document.createElement('video');// made an element of the type video
 ownVideo.muted = true;//muted our own video
 
-let myVideoStream;
+
+const peer = new Peer (undefined); //created a new peer
+
+
+let myOwnVideoStream;
 navigator.mediaDevices.getUserMedia({ //a promise used to access audio and video
     video: true,
     audio: true
   }).then(stream => {
-    myVideoStream = stream;
+    myOwnVideoStream = stream;
     addingVideoStream(ownVideo, stream);
+
+    //ans the call
+peer.on('call', call => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', userVideoStream => {
+        addingVideoStream(video, userVideoStream)
+      })
+    })
+  
+
+    socket.on('user-connected',userId => {
+      connectToAnotherNewUser(userId, stream);
+    })
   })
 
-const addingVideoStream=(video, stream) =>{ //adding it to stream
+  
+
+peer.on('open', id =>{
+  socket.emit('join-room', ID_OF_ROOM, id);
+ }) //we can join the room with that specific room id
+
+  
+
+  const connectToAnotherNewUser = (userId, stream) =>{ //send that new user our video
+ console.log(userId);
+    //we will use peer to connect to our user
+ const call = peer.call(userId, stream)//called our new peer and send our own stream
+ const video = document.createElement('video')
+ call.on('stream', userVideoStream => {
+   addingVideoStream(video, userVideoStream)
+ })
+ 
+  }
+
+ 
+  
+
+  const addingVideoStream=(video, stream) =>{ //adding it to stream
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
       video.play();
     })
-    theVideoGrid.append(video);
+    theVideoGrid.append(video)
   }
--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 
 const express = require('express');// create an express server
 const app = express(); //runs the express function
+
+
 const{v4: uuidV4}=require('uuid');//importing v4 version of uuid
  //uuid would generate random ids for the rooms
 
 const server = require('http').Server(app); //setting up express server to be used with further libraries
+const io = require('socket.io')(server);//importing socket.io
+/*const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
+    }
+})*/
+
+const { ExpressPeerServer } = require('peer');//imported peer
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
+app.use('/peerjs', peerServer);//specific peer which url we are using
 
 app.set('view engine', 'ejs');//our view engine is going to be ejs
 app.use(express.static('public')); //static folder in public url
@@ -17,6 +33,14 @@ app.get('/', (req, res)=>{
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room }) //passed id to the front end
   })
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        console.log("joined room");
+       socket.join(roomId);//joined the room on that specific id
+        socket.to(roomId).emit('user-connected', userId);//telling everyone that a user has been connected
+    })
+    }); 
 
 server.listen(3010); //server will be a local host andd the port will be 3000 
 
